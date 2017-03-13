e8ee4563d88ec4c96c349e50872afdbf153e5ee1
AnshumaanDash <anshumaan.dash@gmail.com>
2017-03-14 00:20:53 +0530

    Add project files.

---
+++ EzyVid/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EzyVid.MvcApplication" Language="C#" %>
---
+++ EzyVid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EzyVid.Startup))]
+namespace EzyVid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
